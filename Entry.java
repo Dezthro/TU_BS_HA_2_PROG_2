e9f1437d225e3daae7d9154bfbb1479a54974e25
Dezthro <mabo7991@gmail.com>
2017-05-21 13:38:38 +0200

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -13,30 +13,22 @@ public class Entry<T> implements Comparable<T> {
     /**
      * the integer array with a length of 3
      */
-    private final Integer[] values;
+    private final Object<T> object;
 
     /**
      * creates a new entry object with the given array
      * @param values the integer array
      */
-    public Entry(final Integer[] values) {
-        if (values == null || values.length != 3) {
+    public Entry(final Object<T> pObject) {
+        if (pObjecr == null) {
             throw new IllegalArgumentException("wrong content");
         }
-        this.values = values;
+        this.object = pObject;
     }
 
     @Override
     public int compareTo(T o) {
         Entry<T> e = (Entry<T>) o;
-        if (Integer.compare(values[0], e.values[0]) == 0) {
-            if (Integer.compare(values[1], e.values[1]) == 0) {
-                return Integer.compare(values[2], e.values[2]);
-            } else {
-                return Integer.compare(values[1], e.values[1]);
-            }
-        }
-        return Integer.compare(values[0], e.values[0]);
     }
 
     @Override
